22b545b9fafa2305a212d6da6ca693c0074e2d23
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-16 21:25:27 +0200

    Update initAdmin.js

--- initAdmin.js
+++ initAdmin.js
@@ -1,7 +1,7 @@
 import sqlite3 from 'sqlite3';
 import bcrypt from 'bcrypt';
 
-const db = new sqlite3.Database('./db/database.sqlite');
+const db = new sqlite3.Database('"./db/scuderia.db');
 
 const username = 'info@battagliaracingcar.com';
 const password = 'Prova1234!';
